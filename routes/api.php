0f98f2d8e54d662706c97cc5f7b32223ff5f3426
Jhon <jhowjhoe@icloud.com>
2025-03-06 19:13:09 -0300

    feat: new router send job

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\JobsController;
 use App\Http\Controllers\UsersController;
 
 Route::group([
@@ -11,6 +12,16 @@ Route::group([
     Route::post('register', [AuthController::class, 'register'])->name('register');
     Route::post('login', [AuthController::class, 'login'])->name('login');
     Route::post('profile', [AuthController::class, 'profile'])->middleware('auth:api')->name('profile');
+
+    Route::post('send', [JobsController::class, 'send'])->middleware('auth:api')->name('send');
+
+});
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'jobs'
+], function ($router) {
+    Route::post('send', [JobsController::class, 'send'])->middleware('auth:api')->name('send');
 });
 
 Route::apiResource('users', UsersController::class)->middleware('auth:api');
